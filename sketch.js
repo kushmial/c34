f1545ca4d8b33815beced9baa307d7eeb7e77b19
kushmial <mitallkush@gmail.com>
2020-11-24 17:00:08 +0530

    Created syncronous ball

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-var ball;
+var ball, position, database;
 
 function setup(){
+    database = firebase.database();
+
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPosRef = database.ref("ball/position");
+    ballPosRef.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
+    if(position !== undefined){
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -20,10 +26,25 @@ function draw(){
     else if(keyDown(DOWN_ARROW)){
         changePosition(0,+1);
     }
+}
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref("ball/position").set({x : position.x + x , y: position.y + y});
+
+   /* ball.x = ball.x + x;
+    ball.y = ball.y + y;*/
+
+
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error Occured");
 }
